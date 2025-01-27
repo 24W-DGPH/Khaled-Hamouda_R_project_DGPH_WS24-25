5ea92bc6bacb3c612195e5e41a26066e5cd80b5e
Khaled-Hamouda <khaled.marzouk.ai@gmail.com>
2025-01-27 14:40:54 +0100

    dataset changed

--- script.R
+++ script.R
@@ -5,37 +5,59 @@ pacman::p_load(
   skimr,          # for reviewing the data
   janitor,        # for cleaning and tabulating data
   epikit,         # for creating age categories
+  kableExtra,     # Build and manipulate complex tables
   tidyverse)      # for data management and visualization
 
 
 # Import dataset ----
-linelist_raw <- import(here("data", "healthcare_dataset.csv"))
+life_exp <- import(here("data", "life_exp.csv"))
+health_xp <- import(here("data", "health_xp.csv"))
 
 # data cleaning ----
+# life_exp
+# add proper column names
+life_exp_columns <- c("country", as.character(1800:2100))
 
-linelist_cleaned <- linelist_raw %>%    # create "clean" dataset, starting with the raw
-  clean_names() %>%                                        # clean the column names
-  distinct() %>%                                           # de-duplicate rows
-  select(
-    name,      gender,   age,   blood_type, 
-         medical_condition,   date_of_admission,     
-            discharge_date,    doctor, hospital, 
-        insurance_provider,      billing_amount,    
-               room_number,      admission_type,        
-                medication,        test_results
-    ) %>%                                                  #re-order columns
-  mutate(
-    discharge_date = as.Date(discharge_date),
-    date_of_admission = as.Date(date_of_admission),
-    hospitalization_duration = as.numeric(discharge_date - date_of_admission)
-    , .after = discharge_date              
-  )                                                        #add column
+# Pivoting data
+life_exp_longer <- life_exp %>%
+  # Add column names
+  setNames(column_names) %>%
+  # Pivot longer all columns except country
+  pivot_longer(
+    cols = -country,
+    names_to = "year",
+    values_to = "value",
+    names_transform = list(year = as.numeric)
+  ) %>%
+  # Filter for years between 1995 and 2010
+  filter(year >= 1995 & year <= 2010) %>%
+  # Remove any rows with NA values
+  drop_na() %>%
+  # Arrange by country and year
+  arrange(country, year)
 
+# health_xp
+# add column names 
+health_xp_columns <- c("country", as.character(1995:2010))
 
-
+# Pivoting data
+health_xp_longer <- health_xp %>%
+  # Add column names
+  setNames(health_xp_columns) %>%
+  # Pivot longer all columns except country
+  pivot_longer(
+    cols = -country,
+    names_to = "year",
+    values_to = "value",
+    names_transform = list(year = as.numeric)
+  ) %>%
+  # Remove any rows with NA values
+  drop_na() %>%
+  # Arrange by country and year
+  arrange(country, year)
 
 
 
 # export cleaned dataset
-write_csv(linelist_cleaned, here("data", "linelist_cleaned.csv"))
+#write_csv(linelist_cleaned, here("data", "linelist_cleaned.csv"))
 
